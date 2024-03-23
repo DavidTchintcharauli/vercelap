f3a0924094b8844f00518a6a88319dc0c0f367cf
DavidTchintcharauli <chincharaulidavid@gmail.com>
2024-03-23 21:31:18 +0400

    add some h1

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php 
-echo "hello from index.php"
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>heelo user</h1>
+    
+</body>
+</html>
